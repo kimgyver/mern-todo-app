8e38bdbe2c767766ce67c2e005978f5c639272c1
unknown <jinyoung777@gmail.com>
2019-10-16 14:21:34 +1300

    set heroku server uri

--- src/env.js
+++ src/env.js
@@ -1,1 +1,1 @@
-export const server_address = 'https://mern-todo-backend-jason.herokuapp.com/' || 'http://localhost';
+export const server_address = 'https://mern-todo-backend-jason.herokuapp.com' || 'http://localhost';
