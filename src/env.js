5b3c6f40369e0337a48dd463f9c3ee50e3090520
unknown <jinyoung777@gmail.com>
2019-10-16 14:42:42 +1300

    heroku env

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
-//export const server_address = 'https://mern-todo-backend-jason.herokuapp.com' || 'http://localhost';
-export const server_address = 'http://localhost';
+export const server_address = 'https://mern-todo-backend-jason.herokuapp.com' || 'http://localhost';
+//export const server_address = 'http://localhost';
